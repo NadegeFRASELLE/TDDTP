172e6b424058c78c34fb0ec05a99e2f9b64abaa8
Nadege <fraselle.nadege@gmail.com>
2024-03-24 20:05:26 +0100

    Premier jet, travail des tests et du code pour couvrir le maximum. Code refactorisé, peut être à renettoyer.

--- src/main/java/org/m1/tdd/ListesNumeriques.java
+++ src/main/java/org/m1/tdd/ListesNumeriques.java
@@ -1,47 +1,222 @@
 package org.m1.tdd;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * La classe ListesNumeriques permet d'ajouter 2 entiers (positifs ou négatifs) représentés en
- * utilisant des listes de chiffres. On ne passera pas par un type <code>Long</code> ou 
+ * utilisant des listes de chiffres. On ne passera pas par un type <code>Long</code> ou
  * <code>BigInteger</code>.
- * 
+ *
  */
 public class ListesNumeriques {
 
+	/**
+	 * La taille maximum de liste qu'on veut accepter.
+	 */
+	final static int SIZE_MAX = 30;
     /**
-     * <p>La méthode considère 2 entiers qui sont représentés en utilisant des listes de 
+     * <p>La méthode considère 2 entiers qui sont représentés en utilisant des listes de
      * chiffres, respectivement <code>nb1</code> et <code>nb2</code>. Cela permet de pouvoir
 	 * manipuler des très grands nombres sans utiliser un type borné tel que <code>Long</code>
 	 * par exemple.
      * La méthode ajoute ces 2 nombres et renvoie le résultat comme une liste de chiffres.
      * </p>
-     * <p>Par exemple, si on veut ajouter les nombres 142 et 13, on doit créer une liste 
+     * <p>Par exemple, si on veut ajouter les nombres 142 et 13, on doit créer une liste
      * (nb1) avec trois éléments [1,4,2] et une liste (nb2) avec deux éléments [1,3].
      * Comme 142+13 = 155, le programme doit donc produire une liste avec trois éléments [1,5,5]
      * </p>
      * [1,4,2] + [1,3] = [1,5,5]
      * <p>
      * Chaque élément des listes nb1 et nb2 doit être compris entre 0 et 9, excepté le premier
-	 * élément de chaque liste qui peut être compris entre -9 et 0 pour pouvoir représenter un nombre 
+	 * élément de chaque liste qui peut être compris entre -9 et 0 pour pouvoir représenter un nombre
 	 * négatif.
-     * Une exception <code>IllegalArgumentException</code> est levée si cette condition préalable n'est 
+     * Une exception <code>IllegalArgumentException</code> est levée si cette condition préalable n'est
      * pas remplie.
      * </p>
-     * 
-     * @param nb1  liste qui contient le premier entier. Si <code>nb1</code> est une liste vide, cela 
+     *
+     * @param nombre1  liste qui contient le premier entier. Si <code>nb1</code> est une liste vide, cela
 	 * correspond à 0. Si <code>nb1</code> est <code>null</code>, le résultat est une liste vide
-     * @param nb2  liste qui contient le deuxième entier. Si <code>nb2</code> est une liste vide, cela 
+     * @param nombre2  liste qui contient le deuxième entier. Si <code>nb2</code> est une liste vide, cela
 	 * correspond à 0. Si <code>nb2</code> est <code>null</code>, le résultat est une liste vide
      * @return la somme de <code>nb1</code> et <code>nb2</code> représentée comme une liste de chiffres
      */
-    public List<Integer> ajoute(List<Integer> nb1, List<Integer> nb2) {
+    public List<Integer> ajoute(List<Integer> nombre1, List<Integer> nombre2) throws Exception {
+   		// vérifications
+		if (nombre1 == null || nombre2 == null || (nombre1.isEmpty() && nombre2.isEmpty())) {
+			return Collections.emptyList();
+		}
+		if (nombre1.isEmpty()){
+			return nombre2;
+		}
+		if (nombre2.isEmpty()){
+			return nombre1;
+		}
+		checkListSize(nombre1, nombre2);
+		checkNumbers(nombre1);
+		checkNumbers(nombre2);
 
-        return Collections.emptyList();
+		//si tout est bon, création de listes modifiables avec la valeur absolue des nombres et renversement des listes.
+		List<Integer> nb1 = new ArrayList<>(getNumAbsValue(nombre1).reversed());
+		List<Integer> nb2 = new ArrayList<>(getNumAbsValue(nombre2).reversed());
+		List<Integer> result;
+		int retenue = 0;
 
+		//si les nombres sont tous deux positifs ou négatifs, on procède à une simple addition avec retenue.
+		if((isNegative(nombre1) && isNegative(nombre2)) || (!isNegative(nombre1) && !isNegative(nombre2))) {
+			result = additionNombres(nb1, nb2, isNegative(nombre1));
+		} else {
+			//sinon, on regarde quel nombre est le plus grand, s'il est négatif pour le signe de la réponse et on lui
+			//soustrait le plus petit.
+			if(getMaxNum(nb1,nb2) == 1) {
+				result = soustractionNombres(nb1, nb2, isNegative(nombre1));
+			} else {
+				result = soustractionNombres(nb2, nb1, isNegative(nombre2));
+			}
+		}
+		return result;
     }
 
+	/**
+	 * Cette méthode est utilisée dans le cadre de l'addition entre un nombre positif et un nombre négatif,
+	 * qui correspond à une soustraction.
+	 *
+	 * @param nb1 le premier nombre, celui auquel on soustrait la valeur de l'autre nombre.
+	 * @param nb2 le deuxième nombre.
+	 * @param nbneg true si le premier nombre, qui est forcément le plus grand, est négatif.
+	 * @return la liste qui contient le résultat de l'opération.
+	 */
+	protected List<Integer> soustractionNombres(List<Integer> nb1, List<Integer> nb2, boolean nbneg) {
+		int retenue = 0;
+		List<Integer> result = new ArrayList<>();
 
+		for (int i = 0; i < Math.max(nb1.size(), nb2.size()); i++) {
+			int num1 = i < nb1.size() ? nb1.get(i) : 0;
+			int num2 = i < nb2.size() ? nb2.get(i) : 0;
+			result.addFirst((num1 < num2 + retenue ? num1 + 10 : num1) - (num2 + retenue));
+			retenue = num1 < (num2 + retenue) ? 1 : 0;
+		}
+		cleanResult(result);
+
+		if (nbneg) {
+			result.set(0, -result.getFirst());
+		}
+		return result;
+	}
+
+	/**
+	 * Cette méthode est utilisée dans le cadre de l'addition entre deux nombres positifs ou deux nombres négatifs.
+	 *
+	 * @param nb1 le premier nombre.
+	 * @param nb2 le deuxième nombre.
+	 * @param nbneg true si on a une addition de nombres négatifs.
+	 * @return la liste qui contient le résultat de l'opération.
+	 */
+	protected List<Integer> additionNombres(List<Integer> nb1, List<Integer> nb2, boolean nbneg) {
+		int retenue = 0;
+		List<Integer> result = new ArrayList<>();
+
+		for(int i = 0 ; i < Math.max(nb1.size(),nb2.size()); i++){
+			int num1 = i < nb1.size() ? nb1.get(i) : 0;
+			int num2 = i < nb2.size() ? nb2.get(i) : 0;
+
+			result.addFirst((num1 + num2 + retenue) > 10 ? (num1 + num2 + retenue - 10) : (num1 + num2 + retenue));
+			retenue = (num1 + num2 + retenue) / 10;
+		}
+		if (retenue != 0) {
+			result.addFirst(retenue);
+		}
+		cleanResult(result);
+		if (nbneg) {
+			result.set(0, -result.getFirst());
+		}
+		return result;
+	}
+
+	/**
+	 * Permet de savoir si un nombre est négatif ou non.
+	 *
+	 * @param nombre le nombre à vérifier.
+	 * @return true si le nombre est négatif, false sinon.
+	 */
+	private boolean isNegative(List<Integer> nombre){
+		return nombre.getFirst() < 0;
+	}
+
+	/**
+	 * Permet de récupérer la valeur absolue d'un nombre qui est sous forme d'une liste d'Integers.
+	 * @param nombre le nombre dont on veut récupérer la valeur absolue.
+	 * @return la liste contenant la valeur absolue qu'on veut.
+	 */
+	private List<Integer> getNumAbsValue(List<Integer> nombre) {
+		List<Integer> ln = new ArrayList<>(nombre);
+		ln.set(0, Math.abs(ln.getFirst()));
+		return ln;
+	}
+
+	/**
+	 * Permet de vérifier si les nombres respectent les contraintes imposées.
+	 * @param nb le nombre à vérifier.
+	 * @throws IllegalArgumentException si le nombre ne respecte pas les contraintes.
+	 */
+	private void checkNumbers(List<Integer> nb) throws IllegalArgumentException {
+		List<Integer> newNb = new ArrayList<>(nb);
+		if(newNb.getFirst() < -9 || (newNb.getFirst() > 9)){
+			throw new IllegalArgumentException("Attention, tous les chiffres doivent être compris entre -9 et 9.");
+		}
+		newNb.removeFirst();
+
+		if (!newNb.stream().allMatch(chiffre -> chiffre >= 0 && chiffre <= 9)) {
+			throw new IllegalArgumentException("Attention, tous les chiffres doivent être compris entre 0 et 9.");
+		}
+	}
+
+	/**
+	 * Méthode utilisée pour savoir quel nombre a la valeur absolue la plus grande pour la soustraction.
+	 *
+	 * @param nb1 le premier nombre à comparer.
+	 * @param nb2 le deuxième nombre à comparer.
+	 * @return l'indice du nombre le plus grand. Retourne 1 si les nombres sont identiques.
+	 */
+	private int getMaxNum(List<Integer> nb1, List<Integer> nb2){
+		if(nb1.size() != nb2.size()){
+			return nb1.size() > nb2.size() ? 1 : 2;
+		}
+		for(int i = nb1.size()-1; i > 0 ; i--){
+			if(!Objects.equals(nb1.get(i), nb2.get(i))){
+				return Objects.equals(Math.max(nb1.get(i), nb2.get(i)),nb1.get(i)) ? 1 : 2;
+			}
+		}
+		return 1;
+
+	}
+
+	/**
+	 * Méthode utilisée pour vérifier que les listes de nombres ne sont pas trop longues.
+	 *
+	 * @param nombre1 le premier nombre dont on veut vérifier la longueur.
+	 * @param nombre2 le deuxième nombre dont on veut vérifier la longueur
+	 * @throws Exception si un des nombres est trop long, ou si les deux sont trop longs.
+	 */
+	private void checkListSize(List<Integer> nombre1, List<Integer> nombre2) throws Exception {
+		if(nombre1.size() > SIZE_MAX || nombre2.size() > SIZE_MAX){
+			throw new Exception("Les listes ne peuvent pas contenir plus de " + SIZE_MAX + " éléments respectivement.");
+		}
+	}
+
+	/**
+	 * Méthode qui permet de nettoyer les résultats en retirant tous les zéros superflus en début de liste.
+	 *
+	 * @param result le nombre à nettoyer.
+	 */
+	private void cleanResult(List<Integer> result){
+		if (result.size() <= 1) {
+			return;
+		}
+		while (result.getFirst() == 0 && result.size() > 1){
+			result.removeFirst();
+		}
+	}
 }
--- src/test/java/org/m1/tdd/ListesNumeriquesTest.java
+++ src/test/java/org/m1/tdd/ListesNumeriquesTest.java
@@ -1,7 +1,14 @@
 package org.m1.tdd;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Tests sur la classe Addition")
@@ -13,5 +20,266 @@ public class ListesNumeriquesTest {
         assertTrue(true);
     }
 
+    @DisplayName("L'addition fonctionne")
+    @Test
+    public void ajouteNombresPositifs() throws Exception{
+        List<Integer> nb1 = Arrays.asList(1, 4, 2);
+        List<Integer> nb2 = Arrays.asList(1, 3);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(1, 5, 5), result);
+    }
+
+    @DisplayName("L'addition fonctionne avec des grands nombres positifs")
+    @Test
+    public void ajouteNombresPositifs2() throws Exception{
+        List<Integer> nb1 = Arrays.asList(9,9,9,9);
+        List<Integer> nb2 = Arrays.asList(9,9,9,9);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(1,9,9,9,8), result);
+    }
+
+    @DisplayName("L'addition fonctionne avec les nombres négatifs")
+    @Test
+    public void testAjouteNombresNegatifs() throws Exception {
+        List<Integer> nb1 = Arrays.asList(-1, 4, 2);
+        List<Integer> nb2 = Arrays.asList(-1, 3);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(-1, 5, 5), result);
+    }
+
+    @DisplayName("L'addition fonctionne avec des grands nombres négatifs")
+    @Test
+    public void testAjouteNombresNegatifs2() throws Exception {
+        List<Integer> nb1 = Arrays.asList(-9, 9, 9, 9);
+        List<Integer> nb2 = Arrays.asList(-9, 9, 9, 9);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(-1,9,9,9,8), result);
+    }
+
+    @DisplayName("L'addition fonctionne avec les deux types de nombres")
+    @Test
+    public void testAjouteNombrePositifEtNegatif() throws Exception {
+        List<Integer> nb1 = Arrays.asList(1, 4, 2);
+        List<Integer> nb2 = Arrays.asList(-1, 3);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(1,2,9), result);
+    }
+
+    @DisplayName("L'addition fonctionne avec les deux types de nombres et le plus gros en 2e")
+    @Test
+    public void testAjouteNombrePositifEtNegatif2() throws Exception {
+        List<Integer> nb1 = Arrays.asList(-1, 4, 2);
+        List<Integer> nb2 = Arrays.asList(5, 2, 3);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(3,8,1), result);
+    }
+
+    @DisplayName("L'addition fonctionne avec les deux types de nombres et le plus gros negatif en 2e")
+    @Test
+    public void testAjouteNombrePositifEtNegatif3() throws Exception {
+        List<Integer> nb1 = Arrays.asList(1, 4, 2);
+        List<Integer> nb2 = Arrays.asList(-5, 2, 3);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(-3,8,1), result);
+    }
+
+    @DisplayName("L'addition fonctionne avec les deux types de nombres et le plus gros positif")
+    @Test
+    public void testAjouteNombrePositifEtNegatif4() throws Exception {
+        List<Integer> nb1 = Arrays.asList(5,2,3);
+        List<Integer> nb2 = Arrays.asList(-1, 4, 2);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(3,8,1), result);
+    }
+
+    @DisplayName("L'addition fonctionne avec les deux types de nombres et des nombres proches")
+    @Test
+    public void testAjouteNombrePositifEtNegatif5() throws Exception {
+        List<Integer> nb1 = Arrays.asList(1, 4, 2);
+        List<Integer> nb2 = Arrays.asList(-1, 7, 0);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(-2,8), result);
+    }
+
+    @DisplayName("L'addition fonctionne avec les deux types de nombres et des nombres proches inversés")
+    @Test
+    public void testAjouteNombrePositifEtNegatif6() throws Exception {
+        List<Integer> nb2 = Arrays.asList(1, 4, 2);
+        List<Integer> nb1 = Arrays.asList(-1, 7, 3);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(-3,1), result);
+    }
+
+    @DisplayName("L'addition fonctionne avec les deux types de nombres et leurs valeurs absolues sont les mêmes")
+    @Test
+    public void testAjouteNombrePositifEtNegatif7() throws Exception {
+        List<Integer> nb1 = Arrays.asList(-1, 4, 2);
+        List<Integer> nb2 = Arrays.asList(1, 4, 2);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(0), result);
+    }
+
+    @DisplayName("L'addition fonctionne avec un premier nombre null")
+    @Test
+    public void testAjouteNombreNull() throws Exception {
+        List<Integer> nb1 = Arrays.asList(1, 4, 2);
+        List<Integer> nb2 = null;
+        ListesNumeriques ln = new ListesNumeriques();
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertTrue(result.isEmpty());
+    }
+
+    @DisplayName("L'addition fonctionne avec un deuxième nombre null")
+    @Test
+    public void testAjouteNombreNull2() throws Exception {
+        List<Integer> nb2 = Arrays.asList(1, 4, 2);
+        List<Integer> nb1 = null;
+        ListesNumeriques ln = new ListesNumeriques();
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertTrue(result.isEmpty());
+    }
+
+    @DisplayName("L'addition fonctionne avec un deuxième nombre null")
+    @Test
+    public void testAjouteNombreNull3() throws Exception {
+        List<Integer> nb1 = null;
+        List<Integer> nb2 = null;
+        ListesNumeriques ln = new ListesNumeriques();
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertTrue(result.isEmpty());
+    }
+
+    @DisplayName("L'addition fonctionne avec un premier nombre invalide")
+    @Test()
+    public void testAjouteNombreInvalide1() {
+        List<Integer> nb1 = Arrays.asList(1, 4, 2, 10);
+        List<Integer> nb2 = Arrays.asList(1, 3);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        assertThatThrownBy(() -> ln.ajoute(nb1, nb2))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("L'addition fonctionne avec un deuxième nombre invalide")
+    @Test()
+    public void testAjouteNombreInvalide2() {
+        List<Integer> nb1 = Arrays.asList(1, 4);
+        List<Integer> nb2 = Arrays.asList(1, 30);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        assertThatThrownBy(() -> ln.ajoute(nb1, nb2))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("L'addition fonctionne quand les deux nombres sont invalides.")
+    @Test()
+    public void testAjouteNombreInvalide3() {
+        List<Integer> nb1 = Arrays.asList(-15, 4);
+        List<Integer> nb2 = Arrays.asList(1, 30);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        assertThatThrownBy(() -> ln.ajoute(nb1, nb2))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("L'addition fonctionne quand la 1ere liste est vide")
+    @Test()
+    public void testAjouteListeVide() throws Exception {
+        List<Integer> nb1 = List.of();
+        List<Integer> nb2 = Arrays.asList(2,6,8);
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(2,6,8), result);
+    }
+
+    @DisplayName("L'addition fonctionne quand la 2e liste est vide")
+    @Test()
+    public void testAjouteListeVide2() throws Exception {
+        List<Integer> nb1 = Arrays.asList(1, 4, 2);
+        List<Integer> nb2 = List.of();
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertEquals(Arrays.asList(1,4,2), result);
+    }
+
+    @DisplayName("L'addition fonctionne quand les deux listes sont vides")
+    @Test()
+    public void testAjouteListeVide3() throws Exception {
+        List<Integer> nb1 = List.of();
+        List<Integer> nb2 = List.of();
+        ListesNumeriques ln = new ListesNumeriques();
+
+        List<Integer> result = ln.ajoute(nb1, nb2);
+        assertTrue(result.isEmpty());
+    }
+
+    @DisplayName("L'addition fonctionne quand le premier nombre est trop long")
+    @Test
+    public void testArrayTooLong1() {
+        List<Integer> nb1 = Arrays.asList(9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4,
+                5, 6, 7, 8, 9, 8);
+        List<Integer> nb2 = Arrays.asList(1,2,3);
+        ListesNumeriques ln = new ListesNumeriques();
+        assertThatThrownBy(() -> ln.ajoute(nb1,nb2))
+                .isExactlyInstanceOf(Exception.class);
+    }
+
+    @DisplayName("L'addition fonctionne quand le deuxième nombre est trop long")
+    @Test
+    public void testArrayTooLong2() {
+        List<Integer> nb2 = Arrays.asList(9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4,
+                5, 6, 7, 8, 9, 8);
+        List<Integer> nb1 = Arrays.asList(1,2,3);
+        ListesNumeriques ln = new ListesNumeriques();
+        assertThatThrownBy(() -> ln.ajoute(nb1,nb2))
+                .isExactlyInstanceOf(Exception.class);
+    }
+
+    @DisplayName("L'addition fonctionne quand le premier nombre contient des chiffres négatifs")
+    @Test
+    public void testNombreNegatifMilieu1(){
+        List<Integer> nb1 = Arrays.asList(1, -4, 2);
+        List<Integer> nb2 = Arrays.asList(1, 4, 2);
+        ListesNumeriques ln = new ListesNumeriques();
+        assertThatThrownBy(() -> ln.ajoute(nb1,nb2))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("L'addition fonctionne quand le deuxième nombre contient des chiffres négatifs")
+    @Test
+    public void testNombreNegatifMilieu2(){
+        List<Integer> nb2 = Arrays.asList(1, -4, 2);
+        List<Integer> nb1 = Arrays.asList(1, 4, 2);
+        ListesNumeriques ln = new ListesNumeriques();
+        assertThatThrownBy(() -> ln.ajoute(nb1,nb2))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
 
 }
+
+
